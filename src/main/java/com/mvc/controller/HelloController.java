15f7613c71ae644d1c9b8077c8111170a70d2c90
psk <psk3060@naver.com>
2019-06-13 21:29:33 +0900

    WAC와 servlet 분리

--- src/main/java/com/mvc/controller/HelloController.java
+++ src/main/java/com/mvc/controller/HelloController.java
@@ -6,9 +6,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequestMapping("/")
 public class HelloController {
     @RequestMapping("/hello")
     public String hello(Model model, @RequestParam(value="name", required = false) String name) {
+        System.out.println("HelloController");
+
         model.addAttribute("greeting", "Hello~" + name + "!!!!");
 
         return "hello";
