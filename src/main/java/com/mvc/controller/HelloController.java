71fe33b1b82f86beaf7cb8d2dd6e69bb3df6bd9f
psk <psk3060@naver.com>
2019-06-13 18:26:34 +0900

    Spring MVC 기본

---
+++ src/main/java/com/mvc/controller/HelloController.java
@@ -0,0 +1,23 @@
+package com.mvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloController {
+    @RequestMapping("/hello")
+    public String hello(Model model, @RequestParam(value="name", required = false) String name) {
+        model.addAttribute("greeting", "Hello~" + name + "!!!!");
+
+        return "hello";
+    }
+    
+    @RequestMapping("/hello2")
+    public String hello2(Model model, @RequestParam(value="name", required = false) String name) {
+        model.addAttribute("greeting", "Hell2o~" + name + "!!!!");
+
+        return "hello";
+    }
+}
