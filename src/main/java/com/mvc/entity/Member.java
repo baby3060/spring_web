ec4bf5a44fe0b01a22e28c1e6b48062a01d3de4c
psk <psk3060@naver.com>
2019-07-15 13:06:23 +0900

    Log 수정 및 Entity 수정

--- src/main/java/com/mvc/entity/Member.java
+++ src/main/java/com/mvc/entity/Member.java
@@ -2,46 +2,64 @@ package com.mvc.entity;
 
 import lombok.*;
 
-@Getter
-@Setter
-@ToString
 @EqualsAndHashCode
-@NoArgsConstructor
-@AllArgsConstructor
 public class Member {
     private String name;
-
-    public String getName() {
-        return this.name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
     private String email;
-
-    public String getEmail() {
-        return email;
-    }
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
     private String password;
-    public String getPassword() {
-        return password;
-    }
-    public void setPassword(String password) {
+    private String confirmPassword;
+    
+    public Member() {}
+    public Member(String name, String email, String password) {
+        this.name = name;
+        this.email = email;
         this.password = password;
     }
-
-    private String confirmPassword;
-
-    public String getConfirmPassword() {
-        return confirmPassword;
-    }
-    public void setConfirmPassword(String confirmPassword) {
+    public Member(String name, String email, String password, String confirmPassword) {
+        this(name, email, password);
         this.confirmPassword = confirmPassword;
     }
+
+    public String toString() {
+        return "[" + this.name + "], email : " + this.email;
+    }
+
+    public boolean equals(Object obj) {
+        if( obj == null ) {
+            return false;
+        } else {
+            if( obj instanceof Member ) {
+                if( obj == this ) {
+                    return true;
+                } else {
+                    Member temp = (Member) obj;
+
+                    return this.name.equals(temp.getName()) && this.email.equals(temp.getEmail()) && this.password.equals(temp.getPassword());
+                }
+            } else {
+                return false;
+            }
+        }
+    }
+
+    public int hashCode() {
+        int result = 31;
+        result += this.name.hashCode();
+        result += this.email.hashCode();
+        result += this.password.hashCode();
+        return result;
+    }
+
+    public String getName() { return this.name; }
+    public void setName(String name) { this.name = name; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+
+    public String getConfirmPassword() { return confirmPassword; }
+    public void setConfirmPassword(String confirmPassword) { this.confirmPassword = confirmPassword; }
+
 }
